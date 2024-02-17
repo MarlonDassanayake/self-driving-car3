df6d522220c1935327c795e63a9855ac7fa383e7
MarlonDassanayake <marlondassanayake06@gmail.com>
2024-02-17 17:11:23 +0000

    a brokem version to be ignored

--- Assets/NNet.cs
+++ Assets/NNet.cs
@@ -2,7 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-using MathNet.Numerics.LinearAlgebra;
 using System;
 
 using Random = UnityEngine.Random;
--- Assets/OutputScripts/Acc1Output.cs
+++ Assets/OutputScripts/Acc1Output.cs
@@ -128,4 +128,3 @@ public class Acc1Output : MonoBehaviour
     }
    
 }
-
